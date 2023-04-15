30c974abe34f8b3b4768f835cbf2f5d68fdb996f
fialovescoding <130829461+fialovescoding@users.noreply.github.com>
2023-04-15 16:02:43 +0530

    Initial basic pandas code

---
+++ basics/data101.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import pandas_ods_reader as por
+
+# Read tables from file
+users = por.read_ods(r"spellbee/data/data.ods", sheet='users')
+users['difficult-words'] = users['difficult-words'].astype(str)
+words = por.read_ods(r"spellbee/data/data.ods", sheet='words')
+
+users.info()
+
+# Filter GIS students and teachers
+# students = users[(users['role'] == 'student') & (users['school-id'] == 1)]
+teachers = users[(users['role'] == 'teacher') & (users['school-id'] == 1)]
+
+# Go thru list of all teachers
+for _, rt in teachers.iterrows():
+    # Print Teacher's Name
+    print(rt['name'])
+
+    # Filter all students of the teacher
+    students = users[(users['role'] == 'student') & \
+                      (users['class'] == rt['class']) & \
+                        (users['section'] == rt['section'])]
+    
+    # Go thru list of all students
+    for _, rs in students.iterrows():
+        print(rs['name'])
+
+        # Get list of difficult word IDs
+        if rs['difficult-words'] is not None:
+            dw = [int(x) for x in rs['difficult-words'].split(',')]
+            difficult_words = words[words['id'].isin(dw)]
+            print(difficult_words)
+    print()
