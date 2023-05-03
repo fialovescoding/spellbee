87a564e247c713cf116d206ad47785bf8a9d2fca
fialovescoding <130829461+fialovescoding@users.noreply.github.com>
2023-05-03 21:31:01 +0530

    minor updates

--- basics/data101.py
+++ basics/data101.py
@@ -6,7 +6,7 @@ users = por.read_ods(r"spellbee/data/data.ods", sheet='users')
 users['difficult-words'] = users['difficult-words'].astype(str)
 words = por.read_ods(r"spellbee/data/data.ods", sheet='words')
 
-users.info()
+# users.info()
 
 # Filter GIS students and teachers
 # students = users[(users['role'] == 'student') & (users['school-id'] == 1)]
@@ -23,12 +23,20 @@ for _, rt in teachers.iterrows():
                         (users['section'] == rt['section'])]
     
     # Go thru list of all students
-    for _, rs in students.iterrows():
-        print(rs['name'])
+    # for _, rs in students.iterrows():
+    #     print(rs['name'])
 
-        # Get list of difficult word IDs
-        if rs['difficult-words'] is not None:
-            dw = [int(x) for x in rs['difficult-words'].split(',')]
-            difficult_words = words[words['id'].isin(dw)]
-            print(difficult_words)
-    print()
+    #     # Get list of difficult word IDs
+    #     if rs['difficult-words'] is not None:
+    #         dw = [int(x) for x in rs['difficult-words'].split(',')]
+    #         difficult_words = words[words['id'].isin(dw)]
+    #         print(difficult_words)
+
+    # print(difficult_words)
+
+min_class = 2
+max_class = 4
+
+my_words = words[(words['min-class'] == 2)]
+
+print(my_words)
